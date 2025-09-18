18539ba3488c74b5ad3dee56f02bfa6643967b16
Micha <pepermicha@protonmail.com>
2025-09-18 13:31:34 +0200

    opdracht 4

--- opdracht 4/opdracht 4.cpp
+++ opdracht 4/opdracht 4.cpp
@@ -1,11 +1,17 @@
 // opdracht 4.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <stdio.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main(void) {
+    char 1;
+
+    printf("Geef een char waarde: ");
+    scanf(" %c", &1);   
+
+    printf("De CHAR waarde van '%c' is: %d\n", 1, (int)1);
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
